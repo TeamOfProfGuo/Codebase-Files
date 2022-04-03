c9bef0c38c94e371ecbc4f789e3f2e055ef8144d
hmdliu <hl3797@nyu.edu>
2022-04-03 10:47:50 -0400

    Update sample code

---
+++ test_gpu.py
@@ -0,0 +1,35 @@
+import time
+import torch
+import warnings
+
+if __name__ == "__main__":
+
+    print(f"Torch cuda available: {torch.cuda.is_available()}")
+    print(f"GPU name: {torch.cuda.get_device_name()}\n\n")
+
+    t1 = torch.randn(100, 1000)
+    t2 = torch.randn(1000, 10000)
+
+    cpu_start = time.time()
+
+    for i in range(100):
+        t = t1 @ t2
+
+    cpu_end = time.time()
+
+    print(f"CPU matmul elapsed: {cpu_end-cpu_start} sec.")
+
+    t1 = t1.to("cuda")
+    t2 = t2.to("cuda")
+
+    gpu_start = time.time()
+
+    for i in range(100):
+        t = t1 @ t2
+
+    gpu_end = time.time()
+
+    print(f"GPU matmul elapsed: {gpu_end-gpu_start} sec.")
+
+    warnings.warn("Sample warning message.")
+
